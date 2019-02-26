5f0daaaedeb6c6f471dc6b9fcec826336988938b
Mitsuhiro Okamoto <mokamoto@salesforce.com>
2019-02-27 01:19:53 +0900

    Merged from Original update

--- force-app/main/default/aura/auraDomEventListener/auraDomEventListenerController.js
+++
@@ -1,6 +0,0 @@
-({
-    handleFilterChange: function(component, event) {
-        var filters = event.getParam('filters');
-        component.set('v.message', filters.length > 0 ? '選択されたもの: ' + filters.join() : '選択されていません');
-    },
-});
